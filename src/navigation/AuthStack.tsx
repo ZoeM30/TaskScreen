7a946d61e9e5fec5a702df6c405622a0e7872448
abdur-raqeeb ajao <ajaorqb@gmail.com>
2023-06-19 23:47:55 -0400

    refactor: navigation stack

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,13 +1,13 @@
 import React, { FC } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { SignUpScreen, LoginScreen } from '../screens';
+import { RegisterScreen, LoginScreen } from '../screens';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const AuthStack: FC = () => {
     return (
         <Navigator screenOptions={{ headerShown: false }}>
-            <Screen name='signup' component={SignUpScreen} />
+            <Screen name='register' component={RegisterScreen} />
             <Screen name='login' component={LoginScreen} />
         </Navigator>
     );
--- src/navigation/MainNav.tsx
+++ src/navigation/MainNav.tsx
@@ -4,7 +4,7 @@ import AuthStack from './AuthStack';
 import AppStack from './AppStack';
 
 const MainNav: FC = () => {
-    const [user, setUser] = useState(null);
+    const [user] = useState(null);
 
     return <NavigationContainer>{user !== null ? <AppStack /> : <AuthStack />}</NavigationContainer>;
 };
