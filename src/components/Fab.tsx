f704d1eb84fc5e936e76c1ec4030250ebf57f03d
Ziyu Ma <Zma5319@conestogac.on.ca>
2023-07-02 12:35:13 -0400

    add fab

--- src/components/Fab.tsx
+++ src/components/Fab.tsx
@@ -1,30 +1,23 @@
 import React, { FC } from 'react';
-import { View,StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { FAB } from '@rneui/themed';
 
-
-
-//https://reactnativeelements.com/docs/components/fab
+// https://reactnativeelements.com/docs/components/fab
 const Fab: FC = () => {
     const [visible] = React.useState(true);
 
     return (
-        <>
         <View style={styles.container}>
-        <FAB
-        visible={visible}
-        icon={{ name: 'add', color: 'white' }}
-        color="#252525"
-        placement='right'
-        />
-      </View>
-      </>)}
+            <FAB visible={visible} icon={{ name: 'add', color: 'white' }} color='#252525' placement='right' />
+        </View>
+    );
+};
 
-const styles=StyleSheet.create({
-    container:{
+const styles = StyleSheet.create({
+    container: {
         alignItems: 'center',
         paddingVertical: 5,
         flexGrow: 1,
     },
-})
+});
 export default Fab;
